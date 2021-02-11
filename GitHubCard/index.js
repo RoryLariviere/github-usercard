eff09cdd0698086cf85802cc2118ae0288e3bea2
Rory Lariviere <rorydlar@gmail.com>
2021-02-11 15:52:26 -0600

    implemented cardMaker function and fed it my github userdata to make myCard

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,22 @@
     https://api.github.com/users/<your name>
 */
 
+import axios from "axios";
+
+// const myCard = axios.get(`https://api.github.com/users/RoryLariviere`);
+const entryPoint = document.querySelector(`.cards`);
+
+// console.log(myCard);
+
+// myCard.then(res => {
+//   cardsHolder.appendChild(cardMaker(res));
+//   console.log(res);
+// })
+// .catch(err => {
+//   console.log(err);
+// })
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -30,31 +46,83 @@
 
 const followersArray = [];
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+function cardMaker(data){
+  const card = document.createElement(`div`);
+  const userImg = document.createElement(`img`);
+  const cardInf = document.createElement(`div`);
+  const nameH3 = document.createElement(`h3`);
+  const userNme = document.createElement(`p`);
+  const locale = document.createElement(`p`);
+  const prof = document.createElement(`p`);
+  const followersCount = document.createElement(`p`);
+  const followingCount = document.createElement(`p`);
+  const bioInf = document.createElement(`p`);
+  console.log(data);
+  card.classList.add(`card`);
+  cardInf.classList.add(`card-info`);
+  nameH3.classList.add(`name`);
+  userNme.classList.add(`username`);
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+  userImg.src = data.avatar_url;
+  nameH3.textContent = data.name;
+  userNme.textContent = data.login;
+  locale.textContent = data.location;
+  prof.textContent = `Profile: `;
+  followersCount.textContent = data.followers;
+  followingCount.textContent = data.following;
+  bioInf.textContent = `Bio: ${data.bio}`;
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+  card.appendChild(userImg);
+  card.appendChild(cardInf);
+  cardInf.appendChild(nameH3);
+  cardInf.appendChild(userNme);
+  cardInf.appendChild(locale);
+  cardInf.appendChild(prof);
+  cardInf.appendChild(followersCount);
+  cardInf.appendChild(followingCount);
+  cardInf.appendChild(bioInf);
+
+
+
+  return card;
+
+}
+
+
+  axios.get(`https://api.github.com/users/RoryLariviere`)
+  .then(res => {
+    const futureData = res.data;
+    entryPoint.appendChild(cardMaker(futureData));
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+
+  // STEP 3: Create a function that accepts a single object as its only argument.
+  //   Using DOM methods and properties, create and return the following markup:
+
+    // <div class="card">
+    //   <img src={image url of user} />
+    //   <div class="card-info">
+    //     <h3 class="name">{users name}</h3>
+    //     <p class="username">{users user name}</p>
+    //     <p>Location: {users location}</p>
+    //     <p>Profile:
+    //       <a href={address to users github page}>{address to users github page}</a>
+    //     </p>
+    //     <p>Followers: {users followers count}</p>
+    //     <p>Following: {users following count}</p>
+    //     <p>Bio: {users bio}</p>
+    //   </div>
+    // </div>
+
+
+
+  // List of LS Instructors Github username's:
+  //   tetondan
+  //   dustinmyers
+  //   justsml
+  //   luishrd
+  //   bigknell
+
